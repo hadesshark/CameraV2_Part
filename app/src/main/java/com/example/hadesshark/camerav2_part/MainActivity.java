a5f1028dd06dafded2575ba899f437f62cbee4ac
王思尹 <hades_shark@hotmail.com>
2017-11-28 20:47:45 +0800

    目前出問題了。

--- app/src/main/java/com/example/hadesshark/camerav2_part/MainActivity.java
+++ app/src/main/java/com/example/hadesshark/camerav2_part/MainActivity.java
@@ -1,13 +1,160 @@
 package com.example.hadesshark.camerav2_part;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextureView mTextureView;
+
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private CameraCaptureSession mCameraCaptureSession;
+    private CameraDevice mCameraDevice;
+    private ImageReader mImageReader;
+    private Handler mCameraHandler;
+    private HandlerThread mCameraThread;
+    private Size mPreviewSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+
+//        mTextureView = (TextureView) findViewById(R.id.textureView);
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        startCameraThread();
+//
+//        if (!mTextureView.isAvailable()) {
+//            mTextureView.setSurfaceTextureListener(mTextureListener);
+//        } else {
+//            startPreview();
+//        }
+//    }
+//
+//    private TextureView.SurfaceTextureListener mTextureListener = new TextureView.SurfaceTextureListener() {
+//        @Override
+//        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+//
+//        }
+//
+//        @Override
+//        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+//
+//        }
+//
+//        @Override
+//        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+//            return false;
+//        }
+//
+//        @Override
+//        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+//
+//        }
+//    };
+//
+//    private void startPreview() {
+//        SurfaceTexture mSurfaceTexture = mTextureView.getSurfaceTexture();
+//        mSurfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+//        Surface previewSurface = new Surface(mSurfaceTexture);
+//    }
+//
+//    private void startCameraThread() {
+//        mCameraThread = new HandlerThread( "CameraThread");
+//        mCameraThread.start();
+//        mCameraHandler = new Handler(mCameraThread.getLooper());
+//
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        if (mCameraCaptureSession != null) {
+//            mCameraCaptureSession.close();
+//            mCameraCaptureSession = null;
+//        }
+//
+//        if (mCameraDevice != null) {
+//            mCameraDevice.close();
+//            mCameraDevice = null;
+//        }
+//
+//        if (mImageReader != null) {
+//            mImageReader.close();
+//            mImageReader = null;
+//        }
+//    }
+//
+//    public void takePicture(View view) {
+//        lockFocus();
+//    }
+//
+//    private void lockFocus() {
+//        try {
+//            mCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER, CameraMetadata.CONTROL_AF_TRIGGER_CANCEL);
+//            mCameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilder.build(), mCaptureCallback, mCameraHandler);
+//        } catch (CameraAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+//        @Override
+//        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+//            super.onCaptureCompleted(session, request, result);
+//            capture();
+//        }
+//    };
+//
+//    private void capture() {
+//        try {
+//            final CaptureRequest.Builder mCaptureBuilder = mCameraDevice.createCaptureRequest(
+//                    CameraDevice.TEMPLATE_STILL_CAPTURE);
+//
+//            mCaptureBuilder.addTarget(mImageReader.getSurface());
+//
+//            CameraCaptureSession.CaptureCallback CaptureCallback = new CameraCaptureSession.CaptureCallback() {
+//                @Override
+//                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+//                    super.onCaptureCompleted(session, request, result);
+//                    unLockFocus();
+//                }
+//            };
+//            mCameraCaptureSession.stopRepeating();
+//            mCameraCaptureSession.capture(mCaptureBuilder.build(), CaptureCallback, null);
+//
+//        } catch (CameraAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void unLockFocus() {
+//    }
 }
